610227753ee1f430f531368e619aea4ad778b1cd
navneet <navneetpalgithub@gmail.com>
2024-10-25 08:51:11 +0530

    updated package permissions

--- app/src/main/java/com/app/exibhitease/presentation/ar_screen/ArScreen.kt
+++ app/src/main/java/com/app/exibhitease/presentation/ar_screen/ArScreen.kt
@@ -1,15 +1,36 @@
 package com.app.exibhitease.presentation.ar_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.exibhitease.R
 import com.app.exibhitease.presentation.data.repository.Art
 import com.app.exibhitease.presentation.viewmodel.SharedViewModel
+import com.app.exibhitease.ui.theme.poppins_regular
+import com.app.exibhitease.ui.theme.poppins_semiBold
+import com.app.exibhitease.ui.theme.system_black
 import com.google.ar.core.Config
 import io.github.sceneview.ar.ARScene
 import io.github.sceneview.ar.node.ArModelNode
@@ -34,6 +55,8 @@ fun ArScreen(
 fun ArDetailScreen(
     art : Art
 ) {
+    
+    var isVisible by remember { mutableStateOf(true) }
     val nodes = remember {
         mutableListOf<ArNode>()
     }
@@ -72,6 +95,46 @@ fun ArDetailScreen(
                 planeRenderer.isVisible = false
             }
         )
+        
+        if (isVisible){
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .background(Color.White, shape = RoundedCornerShape(15.dp))
+                    .align(Alignment.BottomCenter)
+                    .padding(vertical = 10.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Text(
+                        text = art.fullName,
+                        fontSize = 18.sp,
+                        fontFamily = poppins_semiBold,
+                        color = system_black,
+                        modifier = Modifier
+                            .padding(start = 10.dp)
+                    )
+                    IconButton(
+                        onClick = {
+                            isVisible = false
+                        }
+                    ){
+                        Icon(
+                            painter = painterResource(id =R.drawable.close_circle),
+                            contentDescription = null,
+                            tint = Color(0xFF9CA1A7)
+                        )
+                    }
+                }
+
+            }
+        }
 
     }
 }
